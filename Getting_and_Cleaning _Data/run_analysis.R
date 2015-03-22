b651e8666d833124ab313f41d4968b1d70c5985b
ychua9 <bios233@hotmail.com>
2015-03-23 00:05:27 +0800

    Final answer

--- Getting_and_Cleaning _Data/run_analysis.R
+++ Getting_and_Cleaning _Data/run_analysis.R
@@ -17,10 +17,12 @@ run_analysis = function() {
   subject_test = as.data.frame(readLines("UCI HAR Dataset/test/subject_test.txt"))
   colnames(subject_test) = "subject"
   
+  #1. Merges the training and the test sets to create one data set.
   x_merge = rbind(x_train, x_test)
   y_merge = rbind(y_train, y_test)
   subject_merge = rbind(subject_train, subject_test)
   
+  #2. Extracts only the measurements on the mean and standard deviation for each measurement. 
   feature_names = as.data.frame(readLines("UCI HAR Dataset/features.txt"), stringsAsFactors = FALSE)
   colnames(feature_names) = "feature_names" 
   meanStdFeatures = grep("mean|std", feature_names$feature_names)
@@ -33,25 +35,30 @@ run_analysis = function() {
   colnames(measurements) = 1:ncol(measurements)                               
   measurements = t(measurements)
   
+  #3. Uses descriptive activity names to name the activities in the data set
   activity_label = as.data.frame(readLines("UCI HAR Dataset/activity_labels.txt"), stringsAsFactors=FALSE)
   activity_label = as.data.frame(vapply(strsplit(activity_label[,1], " "), function(elem) elem[2], FUN.VALUE=character(1)))
   activitySet = data.frame(activity=1:6, activityLabel=activity_label[,1])
   y_merge$id  = 1:nrow(y_merge)
   y_merge_des = merge(y_merge, activitySet, by="activity", all.x=TRUE)
   y_merge_des = y_merge_des[order(y_merge_des$id),]
-  
+
+  #4. Appropriately labels the data set with descriptive variable names. 
   features_names = vapply(strsplit(feature_names[,1], " "), function(elem) return(elem[2]), FUN.VALUE=character(1))
   feature_names = features_names[meanStdFeatures]
+  for(i in 1:length(feature_names)) {
+    feature_names[i] = paste0(feature_names[i],"-Ave")
+  }
 
+  #5. From the data set in step 4, creates a second, independent tidy data set with 
+  ## the average of each variable for each activity and each subject.
   merged_data = data.table(measurements)
   setnames(merged_data, names(merged_data), feature_names)
   merged_data$subject = as.numeric(subject_merge[,1])
   merged_data$activity = y_merge_des$activityLabel
 
-  mean_by_subject = merged_data[,lapply(.SD,mean),by="subject",.SDcols=1:length(meanStdFeatures)]
-  mean_by_subject = mean_by_subject[order(subject)]
-  mean_by_activity = merged_data[,lapply(.SD,mean),by="activity",.SDcols=1:length(meanStdFeatures)]
-  mean_merge = rbind(mean_by_subject, mean_by_activity, fill=TRUE)
-  write.table(mean_merge, "./tidy_data.txt", row.names=FALSE)
-  
+  mean_by_subjectActivity = merged_data[,lapply(.SD,mean),by="activity,subject",.SDcols=1:length(meanStdFeatures)]
+  mean_by_subjectActivity = mean_by_subjectActivity[order(subject)]
+  write.table(mean_by_subjectActivity, "./tidy_data.txt", row.names=FALSE)
+
 }
